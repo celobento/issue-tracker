6e446a812af8b96a0ac8143780d1675a4f19c811
Marcelo Bento <celobento26@gmail.com>
2024-10-14 22:08:53 -0300

    navbar and styling menu

---
+++ app/NavBar.tsx
@@ -0,0 +1,37 @@
+"use client";
+import classNames from "classnames";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FaBug } from "react-icons/fa";
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { href: "/", label: "Dashboard" },
+    { href: "/issues", label: "Issues" },
+  ];
+  return (
+    <nav className={`flex space-x-6 border-b px-5 mb-5 items-center h-14 `}>
+      <Link href="/">
+        <FaBug />
+      </Link>
+      <ul className={`flex space-x-6`}>
+        {links.map((link) => (
+          <Link
+            key={link.href}
+            className={classNames({
+              "text-stone-900": currentPath === link.href,
+              "text-stone-500": currentPath !== link.href,
+              "hover:text-stone-900 transition-colors": true,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
