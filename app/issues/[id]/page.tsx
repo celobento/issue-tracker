bae4102fa027515cf0a071ef3c2a8ee9a436be2c
Marcelo Bento <celobento26@gmail.com>
2024-10-29 21:13:11 -0300

    marckdown pretty

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,25 +1,36 @@
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
+import ReactMarkdown from "react-markdown";
 import prisma from "../../../prisma/client";
+import IssueStatusBadge from "../IssueStatusBadge";
 
 interface Props {
   params: { id: string };
 }
 
 const IsseDetailPage = async ({ params }: Props) => {
-  if (typeof params.id !== "number") return notFound();
-
-  const issue = await prisma.issue.findUnique({
-    where: {
-      id: parseInt(params.id),
-    },
-  });
-  if (!issue) return notFound();
+  let issue;
+  try {
+    issue = await prisma.issue.findUnique({
+      where: {
+        id: parseInt(params.id),
+      },
+    });
+    if (!issue) return notFound();
+  } catch (error) {
+    return notFound();
+  }
 
   return (
     <div>
-      <p>{issue.title}</p>
-      <p>{issue.description}</p>
-      <p>{issue.status}</p>
+      <Heading>{issue.title}</Heading>
+      <Flex className={`space-x-3 justify-center items-center my-2`}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card className={`prose`}>
+        <ReactMarkdown>{issue.description}</ReactMarkdown>
+      </Card>
     </div>
   );
 };
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,10 @@
 import { Table } from "@radix-ui/themes";
 import delay from "delay";
+import Link from "next/link";
 import prisma from "../../prisma/client";
 import ActionButon from "./ActionButon";
 import IssueStatusBadge from "./IssueStatusBadge";
+
 const IssuePage = async () => {
   const issues = await prisma.issue.findMany();
   await delay(2000);
@@ -24,7 +26,7 @@ const IssuePage = async () => {
           {issues.map((issue) => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
                 <div className={`block md:hidden`}>
                   <IssueStatusBadge status={issue.status} />
                 </div>
