9dc5df9ff81cb5818f0cc7b3e849cda8f64e2ca5
Marcelo Bento <celobento26@gmail.com>
2024-10-31 20:56:53 -0300

    confirmation dialog box

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, Grid } from "@radix-ui/themes";
+import { ResetIcon, TrashIcon } from "@radix-ui/react-icons";
+import { AlertDialog, Box, Button, Flex, Grid } from "@radix-ui/themes";
 import delay from "delay";
 import { notFound } from "next/navigation";
 import prisma from "../../../prisma/client";
@@ -31,7 +32,31 @@ const IsseDetailPage = async ({ params }: Props) => {
       <Box>
         <Flex direction="column" gap="4">
           <IssueEditButton issueId={issue.id} />
-          <IssueDeleteButton issueId={issue.id} />
+          <AlertDialog.Root>
+            <AlertDialog.Trigger>
+              <IssueDeleteButton issueId={issue.id} />
+            </AlertDialog.Trigger>
+            <AlertDialog.Content>
+              <AlertDialog.Title>Confirm delete issue</AlertDialog.Title>
+              <AlertDialog.Description>
+                Are you sure remove this issue. This can't be undone.
+              </AlertDialog.Description>
+              <Flex mt="4" gap="3">
+                <AlertDialog.Cancel>
+                  <Button variant="soft" color="gray">
+                    <ResetIcon />
+                    Cancel
+                  </Button>
+                </AlertDialog.Cancel>
+                <AlertDialog.Action>
+                  <Button color="red">
+                    <TrashIcon />
+                    Delete
+                  </Button>
+                </AlertDialog.Action>
+              </Flex>
+            </AlertDialog.Content>
+          </AlertDialog.Root>
         </Flex>
       </Box>
     </Grid>
